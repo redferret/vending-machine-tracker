7dec3d205036aceb876950da892cfc047e530924
Richard <rdesilvey@gmail.com>
2021-05-31 19:07:16 -0600

    Fix migration and model associations

--- db/migrate/20210601005918_create_snacks.rb
+++ db/migrate/20210601005918_create_snacks.rb
@@ -3,7 +3,7 @@ class CreateSnacks < ActiveRecord::Migration[5.2]
     create_table :snacks do |t|
       t.string :name
       t.float :price
-      t.machine :references
+      t.references :machine, foreign_key: true
 
       t.timestamps
     end
