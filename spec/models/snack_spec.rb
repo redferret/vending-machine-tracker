4e607f2a699a11efb0b7573e6881b2416a260935
Richard <rdesilvey@gmail.com>
2021-05-31 22:29:07 -0600

    Test new instance method

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Snack, type: :model do
     it { should validate_presence_of :price }
     it { should belong_to :machine }
   end
+
+  describe 'instance method,' do
+    describe '#price_formatted' do
+      it 'returns the price of the snack formatted' do
+        owner = FactoryBot.create(:owner)
+        machine = FactoryBot.create(:machine, owner: owner)
+        snack = FactoryBot.create(:snack, machine: machine, price: 2.1)
+
+        expect(snack.price_formatted).to eq '$2.10'
+      end
+    end
+  end
 end
