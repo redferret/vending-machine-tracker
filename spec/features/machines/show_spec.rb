16e1cc52fe0733692721f8e300627f900282bd1c
Richard <rdesilvey@gmail.com>
2021-05-31 19:12:36 -0600

    Add feature test for user story 1

--- spec/features/machines/show_spec.rb
+++ spec/features/machines/show_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe 'The vending machine show page,' do
 
     expect(page).to have_content("#{machine_1.location} Vending Machine")
   end
+
+  it 'shows a list of all the snacks with their names and prices' do
+    owner = FactoryBot.create(:owner)
+    machine_1 = FactoryBot.create(:machine, owner: owner)
+    snack_1 = FactoryBot.create(:snack, machine: machine_1)
+    snack_2 = FactoryBot.create(:snack, machine: machine_1)
+
+    visit machine_path(machine_1)
+
+    within '#snack-list' do
+      expect(page).to have_content("#{snack_1.name} - $#{snack_1.price}")
+      expect(page).to have_content("#{snack_2.name} - $#{snack_2.price}")
+    end
+  end
 end
