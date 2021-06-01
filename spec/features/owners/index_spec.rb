66e5520130ee5c8078dd31b106930f2cf3d0c554
Richard <rdesilvey@gmail.com>
2021-05-31 19:18:49 -0600

    Clean up feature test

--- spec/features/owners/index_spec.rb
+++ spec/features/owners/index_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe 'The owner index page,' do
   it 'shows the list of owners' do
-    FactoryBot.create
+    owner_1 = FactoryBot.create(:owner)
+    owner_2 = FactoryBot.create(:owner)
 
     visit owners_path
 
-    expect(page).to have_content("Sam's Snacks")
-    expect(page).to have_content("Drew's Drinks")
+    expect(page).to have_content(owner_1.name)
+    expect(page).to have_content(owner_2.name)
   end
 end
