09caf3451ecf0f3770d3add210e4e8a8767f51bc
Richard <rdesilvey@gmail.com>
2021-05-31 22:35:50 -0600

    Fix tests

--- spec/features/snacks/show_spec.rb
+++ spec/features/snacks/show_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe 'Show page for a snack,' do
   describe 'vending machine list,' do
     before :all do
       @owner = FactoryBot.create(:owner)
-      @machine_1 = FactoryBot.create(:machine, owner: owner)
-      @machine_2 = FactoryBot.create(:machine, owner: owner)
-      FactoryBot.create(:snack, machine: machine_1)
-      FactoryBot.create(:snack, machine: machine_1)
-      FactoryBot.create(:snack, machine: machine_2)
-      FactoryBot.create(:snack, machine: machine_2)
+      @machine_1 = FactoryBot.create(:machine, owner: @owner)
+      @machine_2 = FactoryBot.create(:machine, owner: @owner)
+      FactoryBot.create(:snack, machine: @machine_1)
+      FactoryBot.create(:snack, machine: @machine_1)
+      FactoryBot.create(:snack, machine: @machine_2)
+      FactoryBot.create(:snack, machine: @machine_2)
     end
-    
+
     it 'shows a list of vending machines the snack is sold at'
     it 'shows vending machines with an average price next to it'
     it 'shows a count of different items at the vending machine'
