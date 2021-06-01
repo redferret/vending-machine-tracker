b45b0c8e49bfac33228fad72f661a35f0e8d6fb2
Richard <rdesilvey@gmail.com>
2021-05-31 22:09:22 -0600

    Add model tests

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Machine, type: :model do
     it { should belong_to :owner }
     it { should have_many :snacks }
   end
+
+  describe 'instance method,' do
+    describe '#average_price' do
+      it 'returns a formatted average of all snack prices' do
+        owner = FactoryBot.create(:owner)
+        machine = FactoryBot.create(:machine, owner: owner)
+        FactoryBot.create(:snack, machine: machine, price: 2)
+        FactoryBot.create(:snack, machine: machine, price: 6)
+        FactoryBot.create(:snack, machine: machine, price: 1)
+
+        expect(machine.average_price).to eq "$3.00"
+      end
+    end
+  end
 end
--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Owner, type: :model do
     it { should validate_presence_of :name }
     it { should have_many :machines }
   end
+
+  describe 'instance method,' do
+    describe '#machine_count' do
+      it 'returns the number of machines the owner has' do
+        owner = FactoryBot.create(:owner)
+        FactoryBot.create(:machine, owner: owner)
+        FactoryBot.create(:machine, owner: owner)
+
+        expect(owner.machine_count).to eq 2
+      end
+    end
+  end
 end
