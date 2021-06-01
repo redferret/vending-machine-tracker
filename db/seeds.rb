15c855ec895930ca6954f6a6c6eb466486e0fdff
Richard <rdesilvey@gmail.com>
2021-05-31 22:09:13 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  owner = FactoryBot.create(:owner)
+  6.times do
+    machine = FactoryBot.create(:machine, owner: owner)
+    10.times do
+      FactoryBot.create(:snack, machine: machine)
+    end
+  end
+end
